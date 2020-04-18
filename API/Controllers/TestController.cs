f9e2971534de86607e57b04258d94bd9daf9f9e3
Kalyani Chintagumpala <kalyani0409@gmail.com>
2020-04-19 01:02:18 +0530

    Add project files.

---
+++ API/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    [Route("[Products]")]
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [SwaggerOperation(operationId: "getA")]
+        public IEnumerable<string> List()
+        {
+            return new string[] { "kalyani", "Ravi" };
+        }
+
+
+        [Route("GetProductByID")]
+        [HttpPost]
+        public IEnumerable<string> GetProductByID(int id)
+        {
+            return new string[] { "kalyani"};
+        }
+    }
+}
---
+++ MyKLearning/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyKLearning.MvcApplication" Language="C#" %>
